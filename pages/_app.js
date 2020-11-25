7cceacd76982e66ddc105d05cc10e65339b3493d
Arjun Biyani <arjun.biyani@ACKO522.local>
2020-11-26 00:57:06 +0530

    Web Vitals init

--- pages/_app.js
+++ pages/_app.js
@@ -4,18 +4,23 @@ export function reportWebVitals(metric) {
     switch (metric.name) {
       case 'FCP':
        console.log("#####FCP######");
+       console.log(metric);
         break
       case 'LCP':
         console.log("#####LCP######");
+        console.log(metric);
         break
       case 'CLS':
         console.log("#####CLS######");
+        console.log(metric);
         break
       case 'FID':
         console.log("#####FID######");
+        console.log(metric);
         break
       case 'TTFB':
         console.log("#####TTFB######");
+        console.log(metric);
         break
       default:
         break
