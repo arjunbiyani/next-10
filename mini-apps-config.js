0e9e1849e5cd3d73896431f2906a410df982379d
Arjun Biyani <arjunbiyani@Arjuns-MacBook-Pro.local>
2021-04-13 22:52:49 +0530

    Multizone

---
+++ mini-apps-config.js
@@ -0,0 +1,14 @@
+const MINIAPPS = {
+  minapp1: {
+    id: 'dashboard',
+    patterns: ['/dashboard'],
+    subPath: '/dashboard'
+  },
+  minapp2: {
+    id: 'storefront',
+    patterns: ['/'],
+    subPath: '/storefront'
+  }
+
+};
+module.exports = MINIAPPS;
