6b8a3d75a30b6360c31887be4449ddbbff45bfd4
AB <arjunbiyani@gmail.com>
2021-06-19 14:12:50 +0530

    tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
-function HomePage() {
-    return <div>Welcome to Next.js!</div>
+import Head from 'next/head'
+
+export default function HomePage() {
+    return <div>
+      <Head>
+        <title>Next 10</title>
+        
+      
+      </Head>
+      <h1>Home</h1>
+      </div>
   }
   
-  export default HomePage
