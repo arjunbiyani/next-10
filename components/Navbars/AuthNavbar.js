0646cca6d84d13cc6acc0c4cd017dc5bd79d6c7b
AB <arjunbiyani@gmail.com>
2021-06-26 18:55:36 +0530

    Setup - 1

---
+++ components/Navbars/AuthNavbar.js
@@ -0,0 +1,71 @@
+import React from "react";
+import Link from "next/link";
+// components
+
+import PagesDropdown from "/components/Dropdowns/PagesDropdown.js";
+
+export default function Navbar(props) {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+  return (
+    <>
+      <nav className="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <Link href="/">
+              <a
+                className="text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+                href="#pablo"
+              >
+              </a>
+            </Link>
+            <button
+              className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <i className="text-white fas fa-bars"></i>
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none" +
+              (navbarOpen ? " block rounded shadow-lg" : " hidden")
+            }
+            id="example-navbar-warning"
+          >
+            <ul className="flex flex-col lg:flex-row list-none mr-auto">
+              <li className="flex items-center">
+               
+              </li>
+            </ul>
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              <li className="flex items-center">
+                <PagesDropdown />
+              </li>
+              <li className="flex items-center">
+             
+              </li>
+
+              <li className="flex items-center">
+                <a
+                  className="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                  href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fdemos.creative-tim.com%2Fnotus-nextjs%2F&text=Start%20your%20development%20with%20a%20Free%20Tailwind%20CSS%20and%20NextJS%20UI%20Kit%20and%20Admin.%20Let%20Notus%20NextJS%20amaze%20you%20with%20its%20cool%20features%20and%20build%20tools%20and%20get%20your%20project%20to%20a%20whole%20new%20level."
+                  target="_blank"
+                >
+                  <i className="lg:text-blueGray-200 text-blueGray-400 fab fa-twitter text-lg leading-lg " />
+                  <span className="lg:hidden inline-block ml-2">Tweet</span>
+                </a>
+              </li>
+
+              <li className="flex items-center">
+               
+              </li>
+
+             
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,35 @@
-import Head from 'next/head'
+/* eslint-disable react/jsx-no-target-blank */
+import React from "react";
+import Link from "next/link";
+
+import IndexNavbar from "../components/Navbars/IndexNavbar.js";
+import Footer from "../components/Footers/Footer.js";
 
 export default function HomePage() {
-    return <div>
-      <Head>
-        <title>Next 10</title>
-        
-      
-      </Head>
-      <h1>Home</h1>
-      </div>
-  }
-  
+  return (
+    <>
+      <IndexNavbar fixed />
+      <section className="header relative pt-16 items-center flex h-screen max-h-860-px">
+        <div className="container mx-auto items-center flex flex-wrap">
+          <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
+            <div className="pt-32 sm:pt-0">
+              <h2 className="font-semibold text-4xl text-blueGray-600">
+                NEXT 10 -@AB
+              </h2>
+              <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
+                Dashboard
+              </p>
+            </div>
+          </div>
+        </div>
+        <img
+          className="absolute top-0 b-auto right-0 pt-16 sm:w-6/12 -mt-48 sm:mt-0 w-10/12 max-h-860-px"
+          src="/img/pattern_nextjs.png"
+          alt="..."
+        />
+      </section>
+
+      <Footer />
+    </>
+  );
+}
