60b0914229c9637cff7bc03f236619f87e6e5a36
Arjun Biyani <arjunbiyani@Arjuns-MacBook-Pro.local>
2021-07-25 23:47:37 +0530

    Tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,7 @@
-// pages/_app.js
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "next/app";
-import Head from "next/head";
-import Router from "next/router";
-import PageChange from "../components/PageChange/PageChange.js";
-import '../styles/globals.css'
+import '../styles/globals.css';
 
-Router.events.on("routeChangeStart", (url) => {
-  console.log(`Loading: ${url}`);
-  document.body.classList.add("body-page-transition");
-  ReactDOM.render(
-    <PageChange path={url} />,
-    document.getElementById("page-transition")
-  );
-});
-Router.events.on("routeChangeComplete", () => {
-  ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
-  document.body.classList.remove("body-page-transition");
-});
-Router.events.on("routeChangeError", () => {
-  ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
-  document.body.classList.remove("body-page-transition");
-});
-
-function MyApp({ Component, pageProps }) {
+  function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
   }
-  
- 
+
   export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,18 @@
 /* eslint-disable react/jsx-no-target-blank */
 import React from "react";
 import Link from "next/link";
+import Head from 'next/head';
+import Image from 'next/image';
 
-import IndexNavbar from "../components/Navbars/IndexNavbar.tsx";
-import Footer from "../components/Footers/Footer.js";
-
-// const getServerSideProps = (context) => {
-//   return(
-//     props:{ context }
-//   )
-// }
-
-const HomePage = () => {
-  return (
-    <>
-      <IndexNavbar user_name="Arjun" fixed />
-      <section className="header relative pt-16 items-center flex h-screen max-h-860-px">
-        <div className="container mx-auto items-center flex flex-wrap">
-          <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
-            <div className="pt-32 sm:pt-0">
-              <h2 className="font-semibold text-4xl text-blueGray-600">
-                NEXT 10 -@AB
-              </h2>
-              <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
-                Dashboard
-              </p>
-            </div>
-          </div>
+const HomePage = () => (
+      <div className="px-8 mt-10">
+        <Head>
+         <title>Bitclass</title>
+        </Head>    
+        <div className="mx-auto">
+         <Image src={'/img/pattern_nextjs.png'} alt={"Picture of the author"} width={1080} height={810}/>
         </div>
-        <img
-          className="absolute top-0 b-auto right-0 pt-16 sm:w-6/12 -mt-48 sm:mt-0 w-10/12 max-h-860-px"
-          src="/img/pattern_nextjs.png"
-          alt="..."
-        />
-      </section>
+      </div>
+);
 
-      <Footer />
-    </>
-  );
-}
 export default HomePage;
